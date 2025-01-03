1d0c8b80d30dd8cee10bf3cd78b8fcf895b5759b
yangalisrinath <srinathnethayangali@gmail.com>
2025-01-03 09:25:43 +0530

    Add files via upload

---
+++ demo2.java
@@ -0,0 +1,12 @@
+public class GreatestOfTwo {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 20;
+
+        if (number1 > number2) {
+            System.out.println("The greatest number is " + number1);
+        } else {
+            System.out.println("The greatest number is " + number2);
+        }
+    }
+}
