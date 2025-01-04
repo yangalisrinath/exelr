8d05e2971839dcf65812c9856d24fa6ad465a83f
yangalisrinath <srinathnethayangali@gmail.com>
2025-01-04 10:35:10 +0530

    Add files via upload

---
+++ demo6.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String username = "SRINATH";
+        String password = "SRINATH@123";
+         while ((username == "SRINATH") && (password == "SRINATH@123")) {
+          System.out.println("hello! SRINATH");
+        break;
+    }
+}
+}
