20fb1e451e31b3f79eb0584ef45fa55e9ffe19aa
yangalisrinath <srinathnethayangali@gmail.com>
2025-01-08 10:22:38 +0530

    Add files via upload

---
+++ demo018.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class demo018 {
+    public static void main(String[] args) {
+        int a;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the multiple thet you want numbers: ");
+        a = sc.nextInt();
+        int result = ((a + 50) / 100) * 100;
+
+        System.out.println("Nearest muntiple number of this number is: " + result);
+
+    }
+
+}
