e20d41cc06ff9bec20ab055effddb4d91e4b96c4
yangalisrinath <srinathnethayangali@gmail.com>
2025-01-27 09:41:03 +0530

    Update and rename demo017.java to Demo017.java

--- Demo017.java
+++ Demo017.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class demo017 {
+public class Demo017 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String loginid = "";
