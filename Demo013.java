f753c3539ac060d3f1281501e79f64b10b3f2a79
yangalisrinath <srinathnethayangali@gmail.com>
2025-01-27 09:39:19 +0530

    Update and rename demo013.java to Demo013.java

--- Demo013.java
+++ Demo013.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class demo013 {
+public class Demo013 {
     public static void main(String[] args) {
         // Create a Scanner object for user input
         Scanner scanner = new Scanner(System.in);
