f24206d2cf066299d3717615d39ef71790de1f7f
yangalisrinath <srinathnethayangali@gmail.com>
2025-01-29 10:28:00 +0530

    Unary Operators

---
+++ Core-Java/Demo014.java
@@ -0,0 +1,12 @@
+
+public class Demo014 {
+    public static void main(String[] args) {
+        int a = 10;
+        a++;
+        a--;
+        System.out.println("Unary operater for postincrement: " + a++);
+        System.out.println("Unary operater for postdecrement: " + a--);
+        System.out.println("Unary operater for predecrement: " + --a);
+        System.out.println("Unary operater for preincrement: " + ++a);
+    }
+}
