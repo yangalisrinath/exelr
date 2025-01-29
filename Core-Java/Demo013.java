19bb312915e29eb7fcaf4d4cf80a694369103bc9
yangalisrinath <srinathnethayangali@gmail.com>
2025-01-29 10:27:11 +0530

    Calculating Area of Triangle

---
+++ Core-Java/Demo013.java
@@ -0,0 +1,9 @@
+public class Demo013 {
+    public static void main(String[] args) {
+        double height = 2;
+        double base = 4;
+        double half = 0.5;
+        double area = half * base * height;
+        System.out.println("Area of triangle: " + area);
+    }
+}
