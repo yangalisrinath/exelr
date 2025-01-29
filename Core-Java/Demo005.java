d358007b56db3195697cfdcd6d528869e4512200
yangalisrinath <srinathnethayangali@gmail.com>
2025-01-29 09:59:46 +0530

    Login Credentials using while loop

---
+++ Core-Java/Demo005.java
@@ -0,0 +1,10 @@
+public class Demo005 {
+    public static void main(String[] args) {
+        String username = "user001";
+        String pwd = "user_001";
+        while ((username == "user001") && (pwd == "user_001")) {
+            System.out.println("Happy New Year");
+            break;
+        }
+    }
+}
