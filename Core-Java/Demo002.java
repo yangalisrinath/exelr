be0cd643687d72f8f266bac6bf9ee7567d65c758
yangalisrinath <srinathnethayangali@gmail.com>
2025-01-29 09:52:32 +0530

    comparing the numbers

---
+++ Core-Java/Demo002.java
@@ -0,0 +1,14 @@
+class Demo002 {
+    public static void main(String[] args) {
+        int num1 = 50;
+        int num2 = 50;
+        if (num1 == num2)
+            System.out.println("Both the numbers  are equal");
+        else if (num1 > num2)
+            System.out.println(num1 + " is greater");
+
+        else
+            System.out.println(num2 + " is greater");
+
+    }
+}
