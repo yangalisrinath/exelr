1f6e4c2628ee65401685687dd5d43ae96fe22688
yangalisrinath <srinathnethayangali@gmail.com>
2025-01-29 10:03:26 +0530

    Add files via upload
    Login Credentials Using while loop when Username and Password is same

---
+++ Core-Java/Demo006.java
@@ -0,0 +1,13 @@
+public class Demo006 {
+    public static void main(String[] args) {
+        String Username;
+        String Password;
+        Username = "srinath";
+        Password = "srinath@123";
+        while ((Username == "srinath") && (Password == "srinath@123")) {
+            System.out.println("Happy new Year");
+            break;
+        }
+
+    }
+}
