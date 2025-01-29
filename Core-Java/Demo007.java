cc4a74e193636904d5716e33169562f73a6fb5c3
yangalisrinath <srinathnethayangali@gmail.com>
2025-01-29 10:07:59 +0530

    Using Scanner Class from User Input

---
+++ Core-Java/Demo007.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Demo007 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your user_name : ");
+        String user_name = scan.nextLine();
+        System.out.print("Enter the phno: ");
+        int phno = (int) scan.nextLong();
+        System.out.println("Enter your age: ");
+        int age = scan.nextInt();
+        System.out.println("Welcome: " + user_name);
+        System.out.println("Your phno is " + phno);
+        System.out.println("Your age is: " + age);
+
+    }
+}
