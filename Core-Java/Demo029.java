b517c1e6a5e5a067484df40aab2829891ef09082
yangalisrinath <srinathnethayangali@gmail.com>
2025-01-30 08:16:26 +0530

    Array Names

---
+++ Core-Java/Demo029.java
@@ -0,0 +1,8 @@
+public class Demo029 {
+    public static void main(String[] args) {
+        String[] frnds = { "srinath", "sai", "sathvik", "shiva", "yash", "madhav", "santhu", "vineesh", "sankar" };
+        for (String friend : frnds) {
+            System.out.println(friend);
+        }
+    }
+}
