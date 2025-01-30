02ae1152ab008355d153683356a197c80bd447c1
yangalisrinath <srinathnethayangali@gmail.com>
2025-01-30 08:06:32 +0530

    Calculating leap year from user input

---
+++ Core-Java/Demo021.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Demo021 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) { // divisible by 4 = 0 and divisible by 100 != 0
+                                                                       // if it satisfies divisible by 400 == 0
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+        scanner.close();
+    }
+}
