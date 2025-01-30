0bec14550ee4973d3e8ef226964f14b1e9c86b4b
yangalisrinath <srinathnethayangali@gmail.com>
2025-01-30 08:02:17 +0530

    Addition of two numbers

---
+++ Core-Java/Demo017.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Demo017 {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the 1st number: ");
+        a = sc.nextInt();
+        System.out.println("Enter the 2nd number: ");
+        b = sc.nextInt();
+        System.out.println("The addition of 2 numbers: " + (a + b));
+    }
+
+}
