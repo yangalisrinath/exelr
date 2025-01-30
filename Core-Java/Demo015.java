e96433e169cd326c84366b3bf7f97fe223de8d36
yangalisrinath <srinathnethayangali@gmail.com>
2025-01-30 07:59:00 +0530

    Swapping of two numbers

---
+++ Core-Java/Demo015.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Demo015 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Before swapping: num1 = " + num1 + ", num2 = " + num2);
+
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        System.out.println("After swapping: num1 = " + num1 + ", num2 = " + num2);
+
+        scanner.close();
+    }
+}
