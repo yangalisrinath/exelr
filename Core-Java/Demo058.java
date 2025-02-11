fef6cce59d09cf53aa3c7b13fef164ed2e6097bb
yangalisrinath <srinathnethayangali@gmail.com>
2025-02-11 18:31:39 +0530

    factors of number

---
+++ Core-Java/Demo058.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Demo058 {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter a number: ");
+            int number = scanner.nextInt();
+            
+            System.out.print("Factors of " + number + " are: ");
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+    }
+}
