bb4fb71c78bd7097e4f54d0f448d1f1218e9fbf5
yangalisrinath <srinathnethayangali@gmail.com>
2025-02-11 18:44:45 +0530

    print index values of an array

---
+++ Core-Java/Demo066.java
@@ -0,0 +1,8 @@
+public class Demo066{
+    public static void main(String[] args){
+        int arr[] = {10,20,30,40,50};
+        for(int i=0;i<arr.length;i++){
+            System.out.println("Index: "+i);
+        }
+    }
+}
