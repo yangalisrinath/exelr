9e7525d74da67de5d1dc022eb842aa40934dbca1
yangalisrinath <srinathnethayangali@gmail.com>
2025-02-11 18:38:19 +0530

    1st,2nd and last element of an array

---
+++ Core-Java/Demo062.java
@@ -0,0 +1,8 @@
+public class Demo062 {
+    public static void main(String[] args){
+        int arr[] = {10,20,30,40,50};
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[arr.length-1]);
+    }
+}
