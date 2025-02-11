ff6645f090a526208defaf94cd52e3ed271a9df5
yangalisrinath <srinathnethayangali@gmail.com>
2025-02-11 18:43:06 +0530

    print array elements

---
+++ Core-Java/Demo065.java
@@ -0,0 +1,8 @@
+public class Demo065 {
+    public static void main(String[] args) {
+        int array[] = {10,20,30,40,50};
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
+}
