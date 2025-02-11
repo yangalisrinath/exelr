01ab5b923f1178a8439106571163ce0b4d995111
yangalisrinath <srinathnethayangali@gmail.com>
2025-02-11 18:50:44 +0530

    prime numbers of an array

---
+++ Core-Java/Demo070.java
@@ -0,0 +1,19 @@
+public class Demo070{
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int num : array) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
