46f113959afa11a27c6fab9c4a3f32d390a5ec17
yangalisrinath <srinathnethayangali@gmail.com>
2025-02-12 09:30:03 +0530

    reverse array elements

---
+++ Core-Java/Demo071.java
@@ -0,0 +1,15 @@
+public class Demo071 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = 0, j = array.length - 1; i < j; i++, j--) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+}
