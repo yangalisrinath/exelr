4e1752344c124f7a44491dde4b515ae832ce042c
yangalisrinath <srinathnethayangali@gmail.com>
2025-02-12 09:32:31 +0530

    linear serach array element

---
+++ Core-Java/Demo072.java
@@ -0,0 +1,25 @@
+class LinearSearch {
+    public int search(int arr[], int N, int x)
+        {
+            for (int i = 0; i < N; i++) {
+                if (arr[i] == x)
+                    return i;
+            }
+            return -1;
+        }
+    }
+    public class Demo072 {
+       
+        public static void main(String args[])
+        {
+            int arr[] = { 2, 3, 4, 10, 40 };
+            int x = 10;
+            LinearSearch obj = new LinearSearch();
+          
+            int result = obj.search(arr, arr.length, x);
+        if (result == -1)
+            System.out.print("Element is not present in array");
+        else
+            System.out.print("Element is present at index "+ result);
+    }
+}
