bea37c013f41b572498f2e9282c41921b70ba688
yangalisrinath <srinathnethayangali@gmail.com>
2025-02-12 09:55:34 +0530

    Reverse integer

---
+++ LeetCode problems/LTCode002.java
@@ -0,0 +1,21 @@
+class LTCode002 {
+    public boolean isPalindrome(int x) {
+       
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversed = 0;
+        int original = x;
+
+       
+        while (x > 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+       
+        return original == reversed;
+    }
+}
